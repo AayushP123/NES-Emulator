cba3f38953906bdb352af1a88956d778b40a6e33
Aayush Pandey <foraayush07@gmail.com>
2026-01-17 23:54:25 -0700

    CPU can fetch, decode, and execute LDA

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,37 @@ struct Cpu {
 }
 fn main() {
     // Mutable CPU values, set start to index of first pc value
-    let mut cpu = Cpu{a : 0, x : 0, y : 0, pc : 0x8000, sp : 0xFD, p : 0x24, mem_buffer : [0;65536]};
+    let mut cpu = Cpu {
+        a: 0,
+        x: 0,
+        y: 0,
+        pc: 0x8000,
+        sp: 0xFD,
+        p: 0x24,
+        mem_buffer: [0; 65536]
+    };
     let start = cpu.pc as usize;
 
-    // tiny program bytes that changes byte values in CPU
+    // load program
     cpu.mem_buffer[start] = 0xA9;
     cpu.mem_buffer[start + 1] = 0x10;
     cpu.mem_buffer[start + 2] = 0x00;
 
+    // Starts loop at "start" opcode
+    loop {
+        let opcode = cpu.mem_buffer[cpu.pc as usize];
+        if (opcode == 0xA9) {
+            cpu.pc += 1;
+            let value = cpu.mem_buffer[cpu.pc as usize];
+            cpu.a = value;
+            cpu.pc += 1;
+            print!("pc = 0x{:04X} and a = {:02X}", cpu.pc, cpu.a);
+        }
+        else if (opcode == 0x00){
+            break;
+    }
+}
+    return;
 
-    // check values
-    println!("{:02X} {:02X} {:02X}",
-             cpu.mem_buffer[start],
-             cpu.mem_buffer[start + 1],
-             cpu.mem_buffer[start + 2],
-    )
+
 }
